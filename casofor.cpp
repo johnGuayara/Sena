707a1cd23fa6c72567fbeacd01d22ceb19249a0b
johnGuayara <88947883+johnGuayara@users.noreply.github.com>
2021-08-18 19:47:39 -0500

    Add files via upload

---
+++ casofor.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+	{
+		int n,i,x,z,numero;	
+			//for(n=1;n<11;n++)
+			cout<<"digite hasta donde quiere que vaya lloviendo en entrada"<<endl;	
+			cin>>x;
+			cout<<"digite hasta donde quiere que vaya la lloviendo"<<endl;	
+			cin>>z;
+			for(n=1;n<=x;n++)
+			{
+				cout<<"esta lloviendo en entrada : "<<n<<endl;
+					for(i=1;i<=z;i++)
+				{
+					cout<<"	lloviendo				: "<<i<<endl;										
+				}
+					
+			}
+				/*cout<<"quede en: "<<n<<endl;
+				for(n=10;n>=1;n--)
+				{
+					cout<<"esta lloviendo en: "<<n<<endl;
+				}
+				cout<<"quede en: "<<n<<endl;*/
+
+			cout<<"quede en lloviendo en: "<<x<<endl;
+	
+		cout<<"quede en entrada"<<i<<endl;
+		cout<<endl;
+		
+		/*for(n=x;n>=1;n--)
+		{
+			cout<<"esta lloviendo en: "<<n<<endl;
+			
+		}
+	
+		cout<<"quede en: "<<n<<endl;*/	
+	cout<<"ingrese en numero desde donde desea iniciar la numeracion "<<endl;
+	cin>>numero;
+	
+	for (n=x;n>=1;n--)
+	{
+		cout<<"esta lloviendo en: "<<n<<endl;
+	}
+	
+	
+	}
